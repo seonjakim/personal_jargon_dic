7a5be8b14d13ada592f838ba93aa7c76c817cb38
seonja <seonja.kim0@gmail.com>
2021-07-23 21:28:25 +0900

    node js test file

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,10 +1,31 @@
 export const jargon = [
   {
-    name: 'Module',
-    meaning: 'hello',
+    name: 'module',
+    meaning:
+      '프로그래밍에서는 프로그램을 구성하는 구성요소의 일부를 의미하는데 프론트엔드 개발에서 어플리케이션 크기가 커지면서 확장성과 유지보수 측면에서 파일을 여러개로 분리하게 되고 이 분리된 파일을 각각 모듈이라고 부름.',
+  },
+  {
+    name: 'package',
+    meaning: '라이브러리와 비슷한 개념',
   },
   {
     name: 'package manager',
-    meaning: 'find',
+    meaning: '패키지를 관리하는 작업을 자동화하고 안전하게 처리하기 위해 사용되는 도구',
   },
+  {
+    name: '패키지 관리',
+    meaning: '패키지를 설치, 업데이트, 수정, 삭제하는 작업 (ex) npm install, uninstall...)',
+  },
+  {
+    name: 'Hypertext Transfer Protocol',
+    meaning: '웹서버와 웹브라우저에서 통신방법을 규정화한',
+  },
+  {
+    name: 'cookie',
+    meaning: '',
+  },
+  // {
+  //   name:'',
+  //   meaning:''
+  // },
 ];
