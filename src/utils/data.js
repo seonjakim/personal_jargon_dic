5eda04ac00262684ddaf28dd424971223312ae76
seonja <seonja.kim0@gmail.com>
2021-07-28 23:20:59 +0900

    module and bundler related def

--- src/utils/data.js
+++ src/utils/data.js
@@ -24,6 +24,99 @@ export const jargon = [
     name: 'cookie',
     meaning: '인터넷 사용자의 컴퓨터에 설치되는 작은 기록 정보 파일',
   },
+  {
+    name: '의존성',
+    meaning:
+      '변경에 의해 영향을 받는 관계 / 한 클래스가 다른 클래스의 메서드를 실행할 때, 의존한다고 함 / 다른 함수 또는 클래스의 변화에 영향받음',
+  },
+  {
+    name: '의존성주입',
+    meaning: '의존하는 객체를 외부에서 넣어주는 패턴 / 객체의 생성과 사용의 관심을 분리',
+  },
+  {
+    name: '모듈',
+    meaning: '분리된 코드조각, 시스템을 이루는 논리적인 일부분',
+  },
+  {
+    name: '모듈 시스템',
+    meaning: '규모가 큰 코드 베이스를 모듈이라는 단위로 분할할 수 있게 만드는 구조',
+  },
+  {
+    name: '모듈화',
+    meaning:
+      '스코프(각 모듈은 독립적인 스코프를 가져야 함), 정의(만들 수 있어야 한다), 사용(가져다 사용할 수 있어야 한다)',
+  },
+  {
+    name: 'common js',
+    meaning:
+      'module.exports를 통해 모듈 정의, require문법을 통해 모듈을 가져다 사용 가능 / 모든 파일은 로컬에 존재함을 전제, 동기적으로 동작',
+  },
+  {
+    name: 'Asynchronous Module Definition',
+    meaning: 'define함수로 파일 스코프 역할을 대신 / 비동기적으로 동작 / 브라우저에서 사용을 중점적으로(?)',
+  },
+  {
+    name: 'Universal Modul Definition',
+    meaning: 'common js와 AMD를 함께 지원하기 위한 디자인 패턴',
+  },
+  {
+    name: '모듈 번들러',
+    meaning:
+      '모든 브라우저에서 ES 모듈을 사용할 수 없으므로 브라우저에서 모듈을 사용하기 위해 모듈의 의존 관계를 미리 해결하여 변환하는 작업 수행',
+  },
+  {
+    name: '로더(loader)',
+    meaning: 'js 파일이 아닌 웹자원(html, css, img etc)의 변환을 도움',
+  },
+  {
+    name: '플러그인(plugin)',
+    meaning: '번들된 결과물을 후처리 / 난독화, 특정 문자열 치환 등의 처리',
+  },
+  {
+    name: '바벨',
+    meaning: '크로싱 브라우저 문제 해결',
+  },
+  {
+    name: 'rollup',
+    meaning: 'ESM을 사용(export, import로 설정파일 작성)',
+  },
+  {
+    name: 'SOP(Same Origin Policy)',
+    meaning: '다른 출처의 리소스 사용을 제한하는 보안방식',
+  },
+  {
+    name: 'CORS(Cross-Origin Resource Sharing)',
+    meaning:
+      'http헤더를 사용, 한 출처에서 실행 중인 웹 어플리케이션이 다른 출처의 자원에 접근 권한을 부여하도록 브라우저에 알려주는 체제',
+  },
+  {
+    name: '출처(origin)란',
+    meaning: 'Url의 Protocol, Host, Port를 통해 같은 출처인지 다른 출처인지 판단',
+  },
+  {
+    name: 'CORS 접근제어 시나리오',
+    meaning: '단순요청(simple request) / Preflight Request / 인증정보 포함 요청(Credentialed Request)',
+  },
+  {
+    name: 'Preflight Request',
+    meaning: '',
+  },
+  // {
+  //   name:'',
+  //   meaning:''
+  // },
+  // {
+  //   name:'',
+  //   meaning:''
+  // },
+  // {
+  //   name:'',
+  //   meaning:''
+  // },
+  // {
+  //   name:'',
+  //   meaning:''
+  // },
   // {
   //   name:'',
   //   meaning:''
